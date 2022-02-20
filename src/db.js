43792b9fec727b36f4e1cd424c27afc9a36409a6
willnorris12 <will.norris@iiksa.com>
2022-02-20 20:46:20 +0000

    feat: connect to mongo cluster

--- src/db.js
+++ src/db.js
@@ -1,8 +1,15 @@
 import mongoose from "mongoose";
 
-export const connectToDatabase = () => {
-    const mongoDB = 'mongodb://127.0.0.1/webshop'
-    mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
-    console.log("Connected to database")
-    mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+export const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+        console.log('Connected to database');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+    // const mongoDB = 'mongodb://127.0.0.1/webshop'
+    // mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
+    // console.log("Connected to database")
+    // mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 }
